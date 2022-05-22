26e3eff6a1c0897d729846fc16d2e194827d419e
NB2010NB <natanbalai1@gmail.com>
2022-05-22 15:04:01 +0300

    js

---
+++ main.js
@@ -0,0 +1,33 @@
+// function returnRandom() {
+//     var randum = Math.random()
+//     return randum
+// } console.log(returnRandom())
+
+
+
+// function returnRandonNumber(num) {
+//     var num = Math.random()
+//     return num
+// } console.log(returnRandonNumber())
+
+
+
+// function printNumberMassege() {
+//     var number = +prompt("INTER NUMBER");
+//     var num1 = Math.floor(Math.random() * 10);
+//     if (number == num1) {
+//         document.write("yess")
+//     }
+//     else {
+//         document.write("Noooo")
+//     }
+// } console.log(printNumberMassege())
+
+
+function popRandomArray() {
+    var RnnArray = [];
+    for (var i = 0; i < 10; i++) {
+        RnnArray.push(Math.floor(Math.random() * 100));
+    }
+    console.log(RnnArray);
+}
